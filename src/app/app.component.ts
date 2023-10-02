26863f014712faa8dc600cf8b9d508d0c5f82aa4
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-10-02 08:54:21 +0200

    Restructuring the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ArtService } from './service/art.service';
 import { Observable } from 'rxjs';
 import { Art } from './interface/art';
+import { Data } from './interface/data';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,20 @@ import { Art } from './interface/art';
 export class AppComponent {
   title = 'dailyartapp';
 
+  art: Art;
+
+  constructor(private artService: ArtService){}
+
+
+  onGetArt():void{
+    this.artService.getArt().subscribe({
+      next: (response) => {
+        this.art = response['data'].filter((value:Data) => value['description'] !== null && value['image_id'] !== null);
+        console.log('Response: ',response)
+        console.log('Assigned value: ', this.art); 
+      },
+      error: (error) =>  console.log(error),
+      complete: ()=> console.log('Done geting Arts')
+    })
+  }
 }
--- src/app/get-art/get-art.component.ts
+++ src/app/get-art/get-art.component.ts
@@ -11,37 +11,8 @@ import { Data } from '@angular/router';
 export class GetArtComponent {
 
 
-  art: Art;
-
-
-  constructor(private artService: ArtService){}
-
-  onGetArt():void{
-    this.artService.getArt().subscribe({
-      next: (response) => {
-        this.art = response['data'].filter((value:Data) => value['description'] !== null && value['image_id'] !== null);
-        console.log('Response: ',response)
-        console.log('Assigned value: ', this.art); 
-      },
-      error: (error) =>  console.log(error),
-      complete: ()=> console.log('Done geting Arts')
-      
-    })
-    
+  onGetArt(){
+    console.log('Button clicked')
   }
-
-
-  // onGetArtItem():void{
-  //   this.artService.getArtItem().subscribe({
-  //     next: (response) => {
-  //       this.art = response;
-  //       console.log('Response: ', response)
-  //       console.log('Assigned value: ',this.art.data);
-  //     },
-  //     error: (error) =>  console.log(error),
-  //     complete: ()=> console.log('Done geting Art')
-      
-  //   })
-  // }
 
 }
