2d5c4ceb7478dd35ec9cc50fe33b21db137bd493
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-10-04 08:19:04 +0200

    Getting randomized artworks from large pool of art

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -13,11 +13,25 @@ export class HttpService {
 
   // https://api.artic.edu/api/v1/artworks/search?q=&fields=id,title,image_id,description
   // https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display
-  private apiUrl = `https://api.artic.edu/api/v1/artworks/search?q=salvador&page=1&limit=100&fields=id,artist_title,title,image_id,description`
+  // Salvador Dali: https://api.artic.edu/api/v1/artworks/search?q=salvador&page=1&limit=100&fields=id,artist_title,title,image_id,description
+  
+  
+  //private apiUrl = `https://api.artic.edu/api/v1/artworks/search?q=salvador&page=1&limit=100&fields=id,artist_title,title,image_id,description`
+
+  private apiUrl : string;
+  randomPage: number;
+  randomLimit: number;
 
   constructor(private http: HttpClient, private artService: ArtService) { }
 
   fetchArt():Observable<Data>{
+
+    this.randomPage = Math.floor((Math.random() * 10) )
+
+    console.log("Random Page: ", this.randomPage)
+
+    this.apiUrl = `https://api.artic.edu/api/v1/artworks/search?q=&page=${this.randomPage}&limit=100&fields=id,artist_title,title,image_id,description`
+
     return this.http.get<Data>(this.apiUrl).pipe(
       tap(art => {
         console.log('Response in http: ', art)
